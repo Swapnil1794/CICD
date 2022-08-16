c09f4d93e6fa8d34c31ee0e62ac681fcad8475f7
Swapnil Gavali <swapnil.gavali@dinebrands.com>
2022-08-16 14:17:32 +0530

    first commit

---
+++ CICD Demo/CICD Demo/CICD_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICD_DemoApp.swift
+//  CICD Demo
+//
+//  Created by M_AMBIN03978 on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CICD_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICD Demo/CICD Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CICD Demo
+//
+//  Created by M_AMBIN03978 on 16/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
